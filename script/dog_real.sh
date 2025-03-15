29e977def9a8e67b7007c226f1ea502e1ab0d3b4
Dyyt587 <805207319@qq.com>
2025-03-15 19:10:09 +0800

    update25_03_15

--- script/dog_real.sh
+++ script/dog_real.sh
@@ -1,3 +1,4 @@
+ros2 daemon start
 colcon build --symlink-install --packages-up-to  unitree_go2_description adog_leg_hardware
 colcon build --packages-up-to ocs2_quadruped_controller 
 colcon build --packages-select ocs2_quadruped_controller 
@@ -5,3 +6,4 @@ colcon build --packages-select hipnuc_imu
 source install/setup.bash 
 ros2 launch hipnuc_imu imu_spec_msg.launch.py
 ros2 launch unitree_go2_description dog_real.launch.py
+ros2 run keyboard_input keyboard_input
